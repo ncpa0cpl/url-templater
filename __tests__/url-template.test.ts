53c5974e1286d7c5780741520c4fc5ca5b4b9545
ncpa0cpl <sbretner@touchbistro.com>
2022-04-12 13:17:02 +0200

    test: added unit tests for relative and absolute url paths

--- __tests__/url-template.test.ts
+++ __tests__/url-template.test.ts
@@ -2,116 +2,313 @@ import { urlTemplate } from "../src";
 
 describe("urlTemplate", () => {
   describe("with positive scenarios", () => {
-    it("should correctly inject single parameter", () => {
-      const template = urlTemplate("http://swapi.dev/api/people/{id}");
+    describe("with an full url", () => {
+      it("should correctly inject single parameter", () => {
+        const template = urlTemplate("http://swapi.dev/api/people/{id}");
 
-      expect(template.generate({ id: 15 })).toEqual(
-        "http://swapi.dev/api/people/15"
-      );
-    });
+        expect(template.generate({ id: 15 })).toEqual(
+          "http://swapi.dev/api/people/15"
+        );
+      });
 
-    it("should correctly inject multiple parameters", () => {
-      const template = urlTemplate(
-        "http://swapi.dev/api/people/{id}/?format={format}"
-      );
+      it("should correctly inject multiple parameters", () => {
+        const template = urlTemplate(
+          "http://swapi.dev/api/people/{id}/?format={format}"
+        );
 
-      expect(template.generate({ id: 69, format: "wookiee" })).toEqual(
-        "http://swapi.dev/api/people/69/?format=wookiee"
-      );
-    });
+        expect(template.generate({ id: 69, format: "wookiee" })).toEqual(
+          "http://swapi.dev/api/people/69/?format=wookiee"
+        );
+      });
 
-    it("should remove white-spaces from the parameters", () => {
-      const template = urlTemplate("http://swapi.dev/api/people/{id}/rest");
+      it("should remove white-spaces from the parameters", () => {
+        const template = urlTemplate("http://swapi.dev/api/people/{id}/rest");
 
-      expect(template.generate({ id: "a  b" })).toEqual(
-        "http://swapi.dev/api/people/a%20%20b/rest"
-      );
-    });
+        expect(template.generate({ id: "a  b" })).toEqual(
+          "http://swapi.dev/api/people/a%20%20b/rest"
+        );
+      });
 
-    it("should correctly inject anything that's string parsable", () => {
-      class StrParsable {
-        toString() {
-          return "foobar";
+      it("should correctly inject anything that's string parsable", () => {
+        class StrParsable {
+          toString() {
+            return "foobar";
+          }
         }
-      }
 
-      const template = urlTemplate(
-        "http://swapi.dev/api/people/{id}/?search={search}"
-      );
+        const template = urlTemplate(
+          "http://swapi.dev/api/people/{id}/?search={search}"
+        );
 
-      expect(
-        template.generate({ id: new StrParsable(), search: [1, "a", 65, true] })
-      ).toEqual("http://swapi.dev/api/people/foobar/?search=1,a,65,true");
+        expect(
+          template.generate({
+            id: new StrParsable(),
+            search: [1, "a", 65, true],
+          })
+        ).toEqual("http://swapi.dev/api/people/foobar/?search=1,a,65,true");
+      });
+
+      it("should correctly inject optional parameter", () => {
+        const template = urlTemplate("http://swapi.dev/api/people/{?id}");
+
+        expect(template.generate({ id: 123 })).toEqual(
+          "http://swapi.dev/api/people/123"
+        );
+      });
+
+      it("should correctly inject multiple optional parameters", () => {
+        const template = urlTemplate(
+          "http://swapi.dev/api/people/{?id}/?search={?search}"
+        );
+
+        expect(template.generate({ id: 123, search: "foo" })).toEqual(
+          "http://swapi.dev/api/people/123/?search=foo"
+        );
+      });
+
+      it("should correctly inject one of optional parameters", () => {
+        const template = urlTemplate(
+          "http://swapi.dev/api/people/{?id}/?search={?search}"
+        );
+
+        expect(template.generate({ id: 123 })).toEqual(
+          "http://swapi.dev/api/people/123/?search="
+        );
+
+        expect(template.generate({ search: "foo" })).toEqual(
+          "http://swapi.dev/api/people/?search=foo"
+        );
+      });
+
+      it("should correctly parse template without any optionals specified", () => {
+        const template1 = urlTemplate("http://swapi.dev/api/people/{?a}/{?b}");
+
+        expect(template1.generate({})).toEqual("http://swapi.dev/api/people");
+
+        const template2 = urlTemplate(
+          "http://swapi.dev/api/people/{?a}/{b}/{?c}"
+        );
+
+        expect(template2.generate({ b: "b" })).toEqual(
+          "http://swapi.dev/api/people/b"
+        );
+      });
+
+      it("should correctly inject optional chained parameter", () => {
+        const template = urlTemplate(
+          "http://swapi.dev/api/people/{?id}/?search={+?search}"
+        );
+
+        expect(template.generate({ search: "r2", id: 20 })).toEqual(
+          "http://swapi.dev/api/people/20/?search=r2"
+        );
+      });
+
+      it("should correctly parse template without any chained optionals specified", () => {
+        const template1 = urlTemplate(
+          "http://swapi.dev/api/people/{+?a}/{+?b}"
+        );
+
+        expect(template1.generate({})).toEqual("http://swapi.dev/api/people");
+
+        const template2 = urlTemplate(
+          "http://swapi.dev/api/people/{+?a}/{b}/{+?c}"
+        );
+
+        expect(template2.generate({ b: "b" })).toEqual(
+          "http://swapi.dev/api/people/b"
+        );
+      });
     });
 
-    it("should correctly inject optional parameter", () => {
-      const template = urlTemplate("http://swapi.dev/api/people/{?id}");
+    describe("with an absolute url", () => {
+      it("should correctly inject single parameter", () => {
+        const template = urlTemplate("/api/people/{id}");
 
-      expect(template.generate({ id: 123 })).toEqual(
-        "http://swapi.dev/api/people/123"
-      );
+        expect(template.generate({ id: 15 })).toEqual("/api/people/15");
+      });
+
+      it("should correctly inject multiple parameters", () => {
+        const template = urlTemplate("/api/people/{id}/?format={format}");
+
+        expect(template.generate({ id: 69, format: "wookiee" })).toEqual(
+          "/api/people/69/?format=wookiee"
+        );
+      });
+
+      it("should remove white-spaces from the parameters", () => {
+        const template = urlTemplate("/api/people/{id}/rest");
+
+        expect(template.generate({ id: "a  b" })).toEqual(
+          "/api/people/a%20%20b/rest"
+        );
+      });
+
+      it("should correctly inject anything that's string parsable", () => {
+        class StrParsable {
+          toString() {
+            return "foobar";
+          }
+        }
+
+        const template = urlTemplate("/api/people/{id}/?search={search}");
+
+        expect(
+          template.generate({
+            id: new StrParsable(),
+            search: [1, "a", 65, true],
+          })
+        ).toEqual("/api/people/foobar/?search=1,a,65,true");
+      });
+
+      it("should correctly inject optional parameter", () => {
+        const template = urlTemplate("/api/people/{?id}");
+
+        expect(template.generate({ id: 123 })).toEqual("/api/people/123");
+      });
+
+      it("should correctly inject multiple optional parameters", () => {
+        const template = urlTemplate("/api/people/{?id}/?search={?search}");
+
+        expect(template.generate({ id: 123, search: "foo" })).toEqual(
+          "/api/people/123/?search=foo"
+        );
+      });
+
+      it("should correctly inject one of optional parameters", () => {
+        const template = urlTemplate("/api/people/{?id}/?search={?search}");
+
+        expect(template.generate({ id: 123 })).toEqual(
+          "/api/people/123/?search="
+        );
+
+        expect(template.generate({ search: "foo" })).toEqual(
+          "/api/people/?search=foo"
+        );
+      });
+
+      it("should correctly parse template without any optionals specified", () => {
+        const template1 = urlTemplate("/api/people/{?a}/{?b}");
+
+        expect(template1.generate({})).toEqual("/api/people");
+
+        const template2 = urlTemplate("/api/people/{?a}/{b}/{?c}");
+
+        expect(template2.generate({ b: "b" })).toEqual("/api/people/b");
+      });
+
+      it("should correctly inject optional chained parameter", () => {
+        const template = urlTemplate("/api/people/{?id}/?search={+?search}");
+
+        expect(template.generate({ search: "r2", id: 20 })).toEqual(
+          "/api/people/20/?search=r2"
+        );
+      });
+
+      it("should correctly parse template without any chained optionals specified", () => {
+        const template1 = urlTemplate("/api/people/{+?a}/{+?b}");
+
+        expect(template1.generate({})).toEqual("/api/people");
+
+        const template2 = urlTemplate("/api/people/{+?a}/{b}/{+?c}");
+
+        expect(template2.generate({ b: "b" })).toEqual("/api/people/b");
+      });
     });
 
-    it("should correctly inject multiple optional parameters", () => {
-      const template = urlTemplate(
-        "http://swapi.dev/api/people/{?id}/?search={?search}"
-      );
+    describe("with an relative url", () => {
+      it("should correctly inject single parameter", () => {
+        const template = urlTemplate("api/people/{id}");
 
-      expect(template.generate({ id: 123, search: "foo" })).toEqual(
-        "http://swapi.dev/api/people/123/?search=foo"
-      );
-    });
+        expect(template.generate({ id: 15 })).toEqual("api/people/15");
+      });
 
-    it("should correctly inject one of optional parameters", () => {
-      const template = urlTemplate(
-        "http://swapi.dev/api/people/{?id}/?search={?search}"
-      );
+      it("should correctly inject multiple parameters", () => {
+        const template = urlTemplate("api/people/{id}/?format={format}");
 
-      expect(template.generate({ id: 123 })).toEqual(
-        "http://swapi.dev/api/people/123/?search="
-      );
+        expect(template.generate({ id: 69, format: "wookiee" })).toEqual(
+          "api/people/69/?format=wookiee"
+        );
+      });
 
-      expect(template.generate({ search: "foo" })).toEqual(
-        "http://swapi.dev/api/people/?search=foo"
-      );
-    });
+      it("should remove white-spaces from the parameters", () => {
+        const template = urlTemplate("api/people/{id}/rest");
 
-    it("should correctly parse template without any optionals specified", () => {
-      const template1 = urlTemplate("http://swapi.dev/api/people/{?a}/{?b}");
+        expect(template.generate({ id: "a  b" })).toEqual(
+          "api/people/a%20%20b/rest"
+        );
+      });
 
-      expect(template1.generate({})).toEqual("http://swapi.dev/api/people");
+      it("should correctly inject anything that's string parsable", () => {
+        class StrParsable {
+          toString() {
+            return "foobar";
+          }
+        }
 
-      const template2 = urlTemplate(
-        "http://swapi.dev/api/people/{?a}/{b}/{?c}"
-      );
+        const template = urlTemplate("api/people/{id}/?search={search}");
 
-      expect(template2.generate({ b: "b" })).toEqual(
-        "http://swapi.dev/api/people/b"
-      );
-    });
+        expect(
+          template.generate({
+            id: new StrParsable(),
+            search: [1, "a", 65, true],
+          })
+        ).toEqual("api/people/foobar/?search=1,a,65,true");
+      });
 
-    it("should correctly inject optional chained parameter", () => {
-      const template = urlTemplate(
-        "http://swapi.dev/api/people/{?id}/?search={+?search}"
-      );
+      it("should correctly inject optional parameter", () => {
+        const template = urlTemplate("api/people/{?id}");
 
-      expect(template.generate({ search: "r2", id: 20 })).toEqual(
-        "http://swapi.dev/api/people/20/?search=r2"
-      );
-    });
+        expect(template.generate({ id: 123 })).toEqual("api/people/123");
+      });
 
-    it("should correctly parse template without any chained optionals specified", () => {
-      const template1 = urlTemplate("http://swapi.dev/api/people/{+?a}/{+?b}");
+      it("should correctly inject multiple optional parameters", () => {
+        const template = urlTemplate("api/people/{?id}/?search={?search}");
 
-      expect(template1.generate({})).toEqual("http://swapi.dev/api/people");
+        expect(template.generate({ id: 123, search: "foo" })).toEqual(
+          "api/people/123/?search=foo"
+        );
+      });
 
-      const template2 = urlTemplate(
-        "http://swapi.dev/api/people/{+?a}/{b}/{+?c}"
-      );
+      it("should correctly inject one of optional parameters", () => {
+        const template = urlTemplate("api/people/{?id}/?search={?search}");
 
-      expect(template2.generate({ b: "b" })).toEqual(
-        "http://swapi.dev/api/people/b"
-      );
+        expect(template.generate({ id: 123 })).toEqual(
+          "api/people/123/?search="
+        );
+
+        expect(template.generate({ search: "foo" })).toEqual(
+          "api/people/?search=foo"
+        );
+      });
+
+      it("should correctly parse template without any optionals specified", () => {
+        const template1 = urlTemplate("api/people/{?a}/{?b}");
+
+        expect(template1.generate({})).toEqual("api/people");
+
+        const template2 = urlTemplate("api/people/{?a}/{b}/{?c}");
+
+        expect(template2.generate({ b: "b" })).toEqual("api/people/b");
+      });
+
+      it("should correctly inject optional chained parameter", () => {
+        const template = urlTemplate("api/people/{?id}/?search={+?search}");
+
+        expect(template.generate({ search: "r2", id: 20 })).toEqual(
+          "api/people/20/?search=r2"
+        );
+      });
+
+      it("should correctly parse template without any chained optionals specified", () => {
+        const template1 = urlTemplate("api/people/{+?a}/{+?b}");
+
+        expect(template1.generate({})).toEqual("api/people");
+
+        const template2 = urlTemplate("api/people/{+?a}/{b}/{+?c}");
+
+        expect(template2.generate({ b: "b" })).toEqual("api/people/b");
+      });
     });
   });
 
